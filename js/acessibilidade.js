c9ef60bf739ea09629d56a0b3136bba85df8ed71
weder96 <weder96@gmail.com>
2017-05-04 02:26:11 -0300

    mudanca acessbilidade

--- js/acessibilidade.js
+++ js/acessibilidade.js
@@ -1,3 +1,74 @@
+//add grupo A
+var tam = 15;
+var tam2=20;
+var testeContraste=0;
+function mudaFonte(tipo,elemento){
+	
+	
+	if (tipo=="mais") {
+		if(tam<21) 
+			tam+=1;
+		createCookie("fonte",tam,365);
+		document.getElementById("txt_home").style.fontSize = tam+"px";
+	} else
+	if (tipo=="menos") {
+		if(tam>12) tam-=1;
+		createCookie("fonte",tam,365);		
+		document.getElementById("txt_home").style.fontSize = tam+"px";
+	}
+	else {
+		tam=15;
+		createCookie("fonte",tam,365);
+		document.getElementById("txt_home").style.fontSize = tam+"px";
+	}	
+}
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	} else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(";");
+	for(var i=0;i < ca.length;i++)
+	{
+		var c = ca[i];
+		while (c.charAt(0)==" "){ 
+			c = c.substring(1,c.length);
+			if(c.indexOf("contraste")>-1){
+				testeContraste=1;					
+			}		
+		}
+		
+		if(testeContraste==1){
+			console.log(testeContraste);
+			$('body').addClass('contraste');
+			createCookie("contraste","contraste",365);
+			e.preventDefault();
+		}
+		
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		
+	}
+	return null;
+}
+
+
+
+function eraseCookie(name)
+{
+createCookie(name,'',-1); 
+console.log(name);
+} 
+
+
+
+
 // Atalhos
 function gotoConteudo(){
     $("#conteudo_ref").focus();
